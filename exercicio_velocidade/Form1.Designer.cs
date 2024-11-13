ccbd8cd7c0d40c60eb85300e706d141a8a2f987a
jpbertti <joaobertty@gmail.com>
2024-11-13 11:51:57 -0300

    Adicionar arquivos de projeto.

---
+++ exercicio_velocidade/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace exercicio_velocidade
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblDistancia = new System.Windows.Forms.Label();
+            this.lblTempo = new System.Windows.Forms.Label();
+            this.txtKm = new System.Windows.Forms.TextBox();
+            this.txtMinutos = new System.Windows.Forms.TextBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(79, 272);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 0;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(267, 272);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblDistancia
+            // 
+            this.lblDistancia.AutoSize = true;
+            this.lblDistancia.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDistancia.Location = new System.Drawing.Point(45, 122);
+            this.lblDistancia.Name = "lblDistancia";
+            this.lblDistancia.Size = new System.Drawing.Size(160, 13);
+            this.lblDistancia.TabIndex = 2;
+            this.lblDistancia.Text = "Distancia Percorrida (KM): ";
+            // 
+            // lblTempo
+            // 
+            this.lblTempo.AutoSize = true;
+            this.lblTempo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTempo.Location = new System.Drawing.Point(45, 176);
+            this.lblTempo.Name = "lblTempo";
+            this.lblTempo.Size = new System.Drawing.Size(109, 13);
+            this.lblTempo.TabIndex = 3;
+            this.lblTempo.Text = "Tempo (Minutos): ";
+            // 
+            // txtKm
+            // 
+            this.txtKm.Location = new System.Drawing.Point(211, 119);
+            this.txtKm.Name = "txtKm";
+            this.txtKm.Size = new System.Drawing.Size(168, 20);
+            this.txtKm.TabIndex = 4;
+            // 
+            // txtMinutos
+            // 
+            this.txtMinutos.Location = new System.Drawing.Point(170, 173);
+            this.txtMinutos.Name = "txtMinutos";
+            this.txtMinutos.Size = new System.Drawing.Size(209, 20);
+            this.txtMinutos.TabIndex = 5;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(1, 67);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(403, 18);
+            this.lblTitulo.TabIndex = 6;
+            this.lblTitulo.Text = "Vamos calcular a Velocidade Média de uma viagem: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 333);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.txtMinutos);
+            this.Controls.Add(this.txtKm);
+            this.Controls.Add(this.lblTempo);
+            this.Controls.Add(this.lblDistancia);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnSair);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblDistancia;
+        private System.Windows.Forms.Label lblTempo;
+        private System.Windows.Forms.TextBox txtKm;
+        private System.Windows.Forms.TextBox txtMinutos;
+        private System.Windows.Forms.Label lblTitulo;
+    }
+}
+
---
+++ exercicio_velocidade/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exercicio_velocidade
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja realmente sair?", "Sair", MessageBoxButtons.YesNo)== DialogResult.Yes)
+                Close();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double Km, vm, tempo;
+          
+            Km = double.Parse(txtKm.Text);
+            tempo = double.Parse(txtMinutos.Text);
+
+            double horas = tempo / 60.0;
+
+            vm = Km / horas;
+
+            MessageBox.Show("A velocidade média da viagem é: " + vm + " Km/h");
+
+        }
+    }
+}
